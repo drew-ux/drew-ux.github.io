ba15cc752fa6d2be1f020da432c56c0669afb48f
Drew <drewallen@Drews-MacBook-Pro.local>
2018-01-23 21:35:47 -0800

    final project update
    changed layout

--- final-project/typography-challenge/js/script.js
+++ final-project/typography-challenge/js/script.js
@@ -2,11 +2,26 @@ $('#start-button').click(
 
 	function () {
 
-		$('#welcome-text').replaceWith('<img class="quiz-img" src="img/helvetica.png">');
+		$('#welcome-text').replaceWith('<div class="card-image"><img class="quiz-img" src="img/helvetica.png"></div>');
 		$('#start-button').hide();
 		$('#btn-grid').css("display", "grid");
 
+		
+		$('#btn-1').addClass("correct");
+		$('#btn-2').addClass("incorrect");
+
+
 	}
 
+
+);
+
+$('.quiz-btn').click(
+
+	function () {
+	
+	$('.quiz-btn').hasClass('incorrect').attr('disabled', 'disabled');
+
+}
 
 );
